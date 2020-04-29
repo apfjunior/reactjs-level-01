e9697c0bb6f72d6c6bbc8428597b679556b80726
Antonino Praxedes <antonino.praxedes@gmail.com>
2020-04-29 00:16:56 -0400

    feat: add introduction css and images with webpack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState }from 'react';
+
+import './App.css';
+import backgroundImage from './assets/harley.jpg'
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProject] = useState(['Desenvolvimento app', 'frontend web']);
+  
+  function handleAddProject() {
+    setProject([...projects, `Novo projeto ${Date.now()}`]);
+  }
+
   return (
     <>
-      <Header />
-      <Header />    
+      <Header title="Projects" />
+      
+      <img width={300} src={backgroundImage} />
+        <ul>
+          {projects.map(project => <li key={project}>{project}</li>)}
+        </ul> 
+
+        <button type="button" onClick={handleAddProject}>Add</button>
     </>
   );
 };
